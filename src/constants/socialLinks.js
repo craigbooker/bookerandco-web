50b666fe19d4b6ea6222feacb451e386849463f0
Craig Booker <817578+craigbooker@users.noreply.github.com>
2021-09-29 11:14:55 -0500

    Updated links to the correct URL.

--- src/constants/socialLinks.js
+++ src/constants/socialLinks.js
@@ -1,30 +1,20 @@
 import React from 'react'
-import {
-  FaFacebookSquare,
-  FaTwitterSquare,
-  FaGithubSquare,
-  FaLinkedin,
-} from 'react-icons/fa'
+import { FaFacebookSquare, FaTwitterSquare, FaLinkedin } from 'react-icons/fa'
 const SocialLinks = ({ styleClass }) => {
   return (
     <ul className={styleClass}>
       <li>
-        <a href="https://twitter.com/craigbooker">
+        <a href="https://twitter.com/bookerandco" target="_blank">
           <FaTwitterSquare className="social-icon twitter-icon"></FaTwitterSquare>
         </a>
       </li>
       <li>
-        <a href="https://github.com/craigbooker">
-          <FaGithubSquare className="social-icon github-icon"></FaGithubSquare>
-        </a>
-      </li>
-      <li>
-        <a href="https://linkedin.com/in/craigbooker">
+        <a href="https://www.linkedin.com/company/bookerandco" target="_blank">
           <FaLinkedin className="social-icon linkedin-icon"></FaLinkedin>
         </a>
       </li>
       <li>
-        <a href="https://facebook.com/craigbooker">
+        <a href="https://www.facebook.com/bookerandcompany" target="_blank">
           <FaFacebookSquare className="social-icon facebook-icon"></FaFacebookSquare>
         </a>
       </li>
